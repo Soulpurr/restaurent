ce234b2b89e9d47b231d97dc42214c2f43c4e009
Soulpurr <abhipoke@gmail.com>
2024-05-11 09:28:31 +0530

    Versioning untracked files

--- components/index.jsx
+++ components/index.jsx
@@ -1,7 +1,8 @@
-import SubHeading from "./SubHeading/SubHeading";
+
 import Newsletter from "./Footer/Newsletter";
 import FooterOverlay from "./Footer/FooterOverlay";
 import MenuItem from "./MenuItem/MenuItem";
 import Navbar from "./Navbar/Navbar";
+import SubHeading from "./SubHeading/SubHeading";
 
 export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
